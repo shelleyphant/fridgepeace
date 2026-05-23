8a1e34236aee5ce52ab17f724fe9141d97fa0a0c
Shelley Bassett <hello@shelleybassett.com>
2026-05-23 14:33:24 +1000

    empty react project

---
+++ client/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+import './main.css';
+
+const App = () => {
+  return <></>;
+};
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
