4c16a483b90a0e7c87a78bba6b495a76a9b03e65
Shelley Bassett <hello@shelleybassett.com>
2026-05-30 10:52:13 +1000

    refactor for consistency

--- client/hooks/useAddFood.js
+++ client/hooks/useAddFood.js
@@ -11,23 +11,26 @@ export function useAddFood() {
     setError(null);
     try {
       if (selected._source === 'foodkeeper') {
-        const body = {
-          foodkeeper_id: selected['ID'].toString(),
-          category: categories[selected['Category_ID']] ?? null,
-          name: selected['Name'],
-          fridge_days_min: selected['Refrigerate_Min'],
-          fridge_days_max: selected['Refrigerate_Max'],
-          freezer_days_min: selected['Freeze_Min'],
-          freezer_days_max: selected['Freeze_Max'],
-          pantry_days_min: selected['Pantry_Min'],
-          pantry_days_max: selected['Pantry_Max'],
-        };
-        await axios.post('/unpackaged-foods', body, {
-          headers: { 'content-type': 'application/json' },
-        });
+        await axios.post('/unpackaged-foods', {
+          foodkeeper_id: selected.ID?.toString() ?? null,
+          category: categories[selected.Category_ID] ?? null,
+          name: selected.Name,
+          fridge_days_min: selected.Refrigerate_Min ?? null,
+          fridge_days_max: selected.Refrigerate_Max ?? null,
+          freezer_days_min: selected.Freeze_Min ?? null,
+          freezer_days_max: selected.Freeze_Max ?? null,
+          pantry_days_min: selected.Pantry_Min ?? null,
+          pantry_days_max: selected.Pantry_Max ?? null,
+        }, { headers: { 'content-type': 'application/json' } });
       } else {
-        // TODO: packaged food
-        console.log(selected);
+        await axios.post('/packaged-foods', {
+          barcode: selected.code ?? null,
+          name: selected.product_name,
+          brand: selected.brands ?? null,
+          image_url: selected.image_front_url ?? null,
+          category: selected.categories ?? null,
+          nutrition: selected.nutriments ? JSON.stringify(selected.nutriments) : null,
+        }, { headers: { 'content-type': 'application/json' } });
       }
     } catch (e) {
       setError(e);
