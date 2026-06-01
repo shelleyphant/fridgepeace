1f1505516e97ce311e815e2059ca89fae9ac4d05
Shelley Bassett <hello@shelleybassett.com>
2026-06-02 06:11:40 +1000

    added a little user tag

--- client/components/inventory/FoodCard.jsx
+++ client/components/inventory/FoodCard.jsx
@@ -7,11 +7,13 @@ const FoodCard = ({ item }) => {
   const Icon = categoryIcon(item.category);
 
   return (
-    <div className="relative rounded-4xl bg-white p-6 pt-4">
+    <div className="relative my-6 rounded-4xl bg-white p-6 pt-4">
       <span className="absolute top-0 left-0 inline-block rounded-4xl bg-amber-200 p-4">
         <HugeiconsIcon icon={Icon} size={32} />
       </span>
+
       <span className="mb-6 ml-14 block text-2xl font-bold">{item.name}</span>
+      <span className="rounded-xl bg-indigo-200 px-2 text-xs">{item.added_by}</span>
       <span className="block">Quantity: {item.quantity}</span>
       <span className="block">
         {item.expiry_date
