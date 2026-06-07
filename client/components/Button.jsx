c72e3632ee0dff368dbc69d9fc2694913521c1ac
Shelley Bassett <hello@shelleybassett.com>
2026-06-07 13:14:37 +1000

    initial onboarding (and button) refinements

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ title, action }) => {
   return (
     <div
-      className="bg-water-800 my-4 rounded-4xl px-6 py-2 text-white hover:cursor-pointer"
+      className="bg-water-800 hover:bg-water-600 my-4 rounded-4xl px-6 py-2 text-center text-lg text-white hover:cursor-pointer"
       onClick={action}
     >
       {title}
