5fe1f637703765c05f79a16eb768ef1743492bd1
Max Jones <mwjon7@student.monash.edu>
2026-06-07 22:37:34 +1000

    Add drawer with app introduction

--- client/components/Onboarding back.jsx
+++ client/components/Onboarding back.jsx
@@ -4,8 +4,7 @@ import Button from './ui/Button';
 import { addHousehold, joinHousehold, getMemberHousehold } from '../hooks/useHousehold';
 import Toast from './ui/Toast';
 import Input from './ui/Input';
-import Drawer from './ui/Drawer';
-import Introduction from './Introduction';
+import AboutDrawer from './AboutDrawer';
 
 const Onboarding = ({ onComplete }) => {
   const [member_id, setMemberId] = useState(localStorage.getItem('member_id'));
@@ -67,28 +66,15 @@ const Onboarding = ({ onComplete }) => {
             <h1 className="font-sansation text-water-800 mb-4 text-5xl font-bold">
               FridgePeace
             </h1>
+
             <h2>Shared pantry management</h2>
             <p>
               We're pretty cool. You can{' '}
-              <Drawer
-                trigger={(open) => (
-                  <a
-                    className="text-water-600 text-center underline hover:cursor-pointer"
-                    onClick={open}
-                  >
-                    learn more here
-                  </a>
-                )}
+              <a className="text-water-600 text-center underline hover:cursor-pointer" 
+              onClick={() => setShowAbout(true)}
               >
-                {(close) => (
-                  <Introduction
-                    onClose={close}
-                    onSuccess={() => {
-                      close();
-                    }}
-                  />
-                )}
-              </Drawer>
+                learn more here
+              </a>
             </p>
           </div>
           <h2 className="text-water-700 text-xl font-medium">Let's get started!</h2>
@@ -99,7 +85,6 @@ const Onboarding = ({ onComplete }) => {
           >
             Or sign up
           </a>
-          <AboutDrawer isOpen={showAbout} onClose={() => setShowAbout(false)} />
         </div>
       );
     return (
