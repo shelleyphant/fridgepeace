429b91e750e2b65337437b100f5195f74ab6ba6f
Shelley Bassett <hello@shelleybassett.com>
2026-06-08 07:50:50 +1000

    basic navigation logic

--- client/components/ui/Navigation.jsx
+++ client/components/ui/Navigation.jsx
@@ -1,71 +1,27 @@
 import React, { useState } from 'react';
 import NavButton from './NavButton';
-import {
-  BellIcon,
-  Cancel01Icon,
-  Logout02Icon,
-  Menu01Icon,
-  Settings01Icon,
-} from '@hugeicons/core-free-icons';
-import Notifications from '../user/Notifications';
-import Settings from '../user/Settings';
-import Drawer from './Drawer';
-import { logOut } from '../../hooks/useMembership';
+import { Menu01Icon } from '@hugeicons/core-free-icons';
 
-const Navigation = ({ onReset }) => {
+const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="absolute top-6 right-6 flex flex-col items-end">
+    <nav>
       <NavButton
-        name="Menu"
-        icon={isOpen ? Cancel01Icon : Menu01Icon}
+        name="menu"
+        icon={Menu01Icon}
         action={() => {
           setIsOpen(isOpen ? false : true);
         }}
       />
       <ul className={isOpen ? `flex` : `hidden`}>
         <li>
-          <Drawer
-            trigger={(open) => (
-              <NavButton name="Notifications" icon={BellIcon} action={open} />
-            )}
-          >
-            {(close) => (
-              <Notifications
-                onClose={close}
-                onSuccess={() => {
-                  close();
-                }}
-              />
-            )}
-          </Drawer>
+          <NavButton />
         </li>
         <li>
-          <Drawer
-            trigger={(open) => (
-              <NavButton name="Settings" icon={Settings01Icon} action={open} />
-            )}
-          >
-            {(close) => (
-              <Settings
-                onClose={close}
-                onSuccess={() => {
-                  close();
-                }}
-                onReset={onReset}
-              />
-            )}
-          </Drawer>
+          <NavButton />
         </li>
         <li>
-          <NavButton
-            name="Log Out"
-            icon={Logout02Icon}
-            action={() => {
-              logOut();
-              onReset();
-            }}
-          />
+          <NavButton />
         </li>
       </ul>
     </nav>
