1bb20778ca31119e73d3c2e1cbe421ba18e4d00f
Shelley Bassett <hello@shelleybassett.com>
2026-06-08 15:25:29 +1000

    Added the drawer and placeholder components

--- client/components/user/Settings.jsx
+++ client/components/user/Settings.jsx
@@ -1,30 +1,7 @@
 import React from 'react';
-import Button from '../ui/Button';
-import { leaveHousehold } from '../../hooks/useHousehold';
-import Modal from '../ui/Modal';
 
-const Settings = ({ onReset }) => {
-  return (
-    <div>
-      <Modal trigger={(open) => <Button title="Leave household" color="red" action={open} />}>
-        {(close) => (
-          <div>
-            <p>are you sure you want to leave the household?</p>
-            <a onClick={close}>Cancel</a>
-            <a
-              onClick={() => {
-                leaveHousehold();
-                onReset();
-                close();
-              }}
-            >
-              Leave Household
-            </a>
-          </div>
-        )}
-      </Modal>
-    </div>
-  );
+const Settings = () => {
+  return <div>Settings</div>;
 };
 
 export default Settings;
