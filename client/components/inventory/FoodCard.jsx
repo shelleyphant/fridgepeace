c230862d5e8c89f8dc32111249fb83d5ef2880bb
Shelley Bassett <hello@shelleybassett.com>
2026-06-10 08:19:23 +1000

    colour variation to card icons

--- client/components/inventory/FoodCard.jsx
+++ client/components/inventory/FoodCard.jsx
@@ -6,9 +6,26 @@ import { categoryIcon } from '../../source/categoryIcons';
 const FoodCard = ({ item }) => {
   const Icon = categoryIcon(item.category);
 
+  const expiryColor = () => {
+    if (!item.expiry_date) return '';
+    const days = moment(item.expiry_date, 'YYYY-MM-DD').diff(moment(), 'days');
+    switch (true) {
+      case days < 0:
+        return 'bg-red-200';
+      case days <= 2:
+        return 'bg-orange-200';
+      case days <= 10:
+        return 'bg-amber-200';
+      default:
+        return 'bg-lime-200';
+    }
+  };
+
   return (
     <div className="relative my-6 rounded-4xl bg-white p-6 pt-4">
-      <span className="absolute top-0 left-0 inline-block rounded-4xl bg-amber-200 p-4">
+      <span
+        className={`rounded-4xl ${expiryColor()} absolute top-0 left-0 inline-block p-4`}
+      >
         <HugeiconsIcon icon={Icon} size={32} />
       </span>
 
@@ -20,7 +37,7 @@ const FoodCard = ({ item }) => {
         </span>
       )}
       <span className="block">Quantity: {item.quantity}</span>
-      <span className="block">
+      <span className={`inline-block rounded-xl px-2 text-xs`}>
         {item.expiry_date
           ? `Expires ${moment(item.expiry_date, 'YYYY-MM-DD').fromNow()}`
           : 'No expiry set'}
