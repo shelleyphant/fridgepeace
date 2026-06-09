98808b6cd981e0028e71572356cab445a5ba20cf
Shelley Bassett <hello@shelleybassett.com>
2026-06-10 08:10:00 +1000

    capital letter 🙄

--- client/components/user/Settings.jsx
+++ client/components/user/Settings.jsx
@@ -6,10 +6,12 @@ import Modal from '../ui/Modal';
 const Settings = ({ onReset }) => {
   return (
     <div>
-      <Modal trigger={(open) => <Button title="Leave household" color="red" action={open} />}>
+      <Modal
+        trigger={(open) => <Button title="Leave household" color="red" action={open} />}
+      >
         {(close) => (
           <div>
-            <p>are you sure you want to leave the household?</p>
+            <p>Are you sure you want to leave the household?</p>
             <a onClick={close}>Cancel</a>
             <a
               onClick={() => {
