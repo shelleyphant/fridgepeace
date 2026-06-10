17f6e8d866de5c4252974d6e37dd30a9f5ca2f05
Shelley Bassett <hello@shelleybassett.com>
2026-06-10 20:56:46 +1000

    refactor for the stupid edit drawer

--- client/components/MainInventory.jsx
+++ client/components/MainInventory.jsx
@@ -1,42 +1,15 @@
 import React, { useState } from 'react';
-import Button from './ui/Button';
-import Drawer from './ui/Drawer';
 import FoodCard from './inventory/FoodCard';
 import FoodCardStack from './inventory/FoodCardStack';
 import Toast from './ui/Toast';
-import { useHousehold } from '../hooks/useHousehold';
 import { useInventory } from '../hooks/useInventory';
-import AddFood from './inventory/AddFood';
 
 const MainInventory = () => {
   const [toast, setToast] = useState(null);
   const { inventory, loading, refresh } = useInventory();
-  const household = useHousehold(localStorage.getItem('household_id'));
 
   return (
-    <>
-      <h1 className="text-water-800 font-sansation text-4xl font-bold">
-        {household?.name}
-      </h1>
-      <span>{household?.id}</span>
-      <hr className="h-8 border-0" />
-      <Drawer trigger={(open) => <Button title="New Food" action={open} />}>
-        {(close) => (
-          <AddFood
-            onClose={close}
-            onSuccess={() => {
-              refresh();
-              close();
-              setToast({
-                id: Date.now(),
-                level: 'success',
-                message: 'Food added to kitchen!',
-              });
-            }}
-          />
-        )}
-      </Drawer>
-
+    <div className="mt-40">
       {!loading &&
         Object.values(
           inventory.reduce((groups, item) => {
@@ -64,7 +37,7 @@ const MainInventory = () => {
         )}
 
       {toast && <Toast key={toast.id} level={toast.level} message={toast.message} />}
-    </>
+    </div>
   );
 };
 
